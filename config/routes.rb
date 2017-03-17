1c966f56d027031a1681abe4363bf0985bea3793
Adrian Diaz <web.art@adriandiaz.ca>
2017-03-16 18:22:21 -0700

    sad empty cart cat

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
+
   resource :cart, only: [:show] do
     put    :add_item
     delete :remove_item
