1304da230dcdd68714732ce8ddc29ea9787b5146
Adrian Diaz <web.art@adriandiaz.ca>
2017-03-17 17:58:34 -0700

    mailer

--- app/mailers/jungle_mailer.rb
+++ app/mailers/jungle_mailer.rb
@@ -1,9 +1,10 @@
 class JungleMailer < ApplicationMailer
   default from: "no-reply@jungle.com"
 
-  def purchase_confirmation(user)
-    @user = user
-    mail(to: @user.email, subject: 'Sample Email')
+  def purchase_confirmation(order)
+    @order = order
+    mail(to: @order.email, subject: 'Sample Email')
   end
+
 
 end
